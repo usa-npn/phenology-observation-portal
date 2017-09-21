aa4064016466500df6ae5c09c3944b3f175d5ab6
Lee Marsh <npnlee85@gmail.com>
2017-09-21 10:35:16 -0700

    Adding HTTPS fix

--- app/config.service.ts
+++ app/config.service.ts
@@ -4,30 +4,39 @@ import {Injectable} from '@angular/core';
 export class Config {
 
     public getPopServerUrl() {
-        if(location.hostname.includes('local'))
+        if(location.hostname.includes('local')){
             return location.protocol + '//' + location.hostname;
-        if(location.hostname.includes('dev'))
-            return location.protocol + "//data-dev.usanpn.org";
-        else
-            return location.protocol + "//data.usanpn.org";
+        }
+        
+        if(location.hostname.includes('dev')){
+            return "https://data-dev.usanpn.org";
+        }else{
+            return "https://data.usanpn.org";
+        }
     }
 
     public getNpnPortalServerUrl() {
-        if(location.hostname.includes('local'))
+        if(location.hostname.includes('local')){
             return location.protocol + '//' + location.hostname;
-        if(location.hostname.includes('dev'))
-            return location.protocol + "//www-dev.usanpn.org";
-        else
-            return location.protocol + "//www.usanpn.org";
+        }
+        
+        if(location.hostname.includes('dev')){
+            return "https://www-dev.usanpn.org";
+        }else{
+            return "https://www.usanpn.org";
+        }
     }
 
     public getPopUrl() {
-        if(location.hostname.includes('local'))
+        if(location.hostname.includes('local')){
             return window.location.origin;
-        if(location.hostname.includes('dev'))
+        }
+        
+        if(location.hostname.includes('dev')){
             return window.location.origin + "/observations";
-        else
+        }else{
             return window.location.origin + "/observations";
+        }
     }
     
     public getPopDownloadEndpoint() {
