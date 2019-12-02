1d0a06e9d941fdd40a98fcc1a9ebcb0aa4d8318b
Kevin Wong <kevin@usanpn.org>
2019-12-02 13:04:28 -0700

    small fix

--- app/integrated-datasets/integrated-datasets.service.ts
+++ app/integrated-datasets/integrated-datasets.service.ts
@@ -38,12 +38,17 @@ export class IntegratedDatasetService {
                 || d.dataset_name === 'ADF Nature Log'
                 || d.dataset_name === 'NYBG 2009-2013'
                 || d.dataset_name === 'GRSM Tremont 2010-2012'
-                || d.dataset_name === 'NEON 2013-Present');
+                // || d.dataset_name === 'NEON 2013-Present'
+                );
 
+                let neonDataset = datasets.filter((d) => d.dataset_name === 'NEON 2013-Present');
                 // move neon data to be second
                 console.log(datasets);
-                datasets.splice(0,0,datasets.splice(5, 1)[0]);
- 
+                // datasets.splice(0,0,datasets.splice(5, 1)[0]);
+                datasets = neonDataset.concat(datasets)
+
+                console.log(datasets);
+
                 var nnDataset = <Dataset>{};
                 nnDataset.dataset_id = -9999;
                 nnDataset.dataset_name= 'Nature\'s Notebook';  
