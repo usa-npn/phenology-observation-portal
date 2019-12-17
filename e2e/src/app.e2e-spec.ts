f0d2732290603968b408d47f09e2d9b3354b5405
Jeff Switzer <jeff.switzer@gmail.com>
2019-12-17 12:18:16 -0700

    project name update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to test-angular-proj!');
+    expect(page.getTitleText()).toEqual('Welcome to phenology-observation-portal!');
   });
 
   afterEach(async () => {
--- src/app/old/app.component.ts
+++ src/app/old/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-angular-proj';
+  title = 'phenology-observation-portal';
 }
